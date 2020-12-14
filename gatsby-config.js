e9da164fbf256b54544b0da68ad85f5ea705048a
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-12-14 05:20:16 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
         // This is the field under which it's accessible
         fieldName: "LOLLIES",
         // URL to query from
-        url: "https://muhibullahlollygift.netlify.app/.netlify/functions/newLolly",
+        url: "/.netlify/functions/newLolly",
       },
     },
 
