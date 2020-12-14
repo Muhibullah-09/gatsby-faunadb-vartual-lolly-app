3d2304cd9064abb6187a3944fffae9ed8440680f
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-12-15 00:43:21 +0500

    add code

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -22,9 +22,7 @@ export default function NotFound({ location }) {
   var queryLollies = location.pathname.slice(0, 9)
   var queryPath = location.pathname.slice(9)
 
-  const { loading, error, data } = useQuery(GET_LOLLY_BY_PATH, {
-    variables: { lollyPath: queryPath },
-  })
+  const { loading, error, data } = useQuery(GET_LOLLY_BY_PATH, { variables: { lollyPath: queryPath }})
 
   return (
     <div>
@@ -39,7 +37,7 @@ export default function NotFound({ location }) {
           <h5 className="sharableLinkContainer">Your sharable link: </h5>{" "}
           <span className="sharableLink">
             {" "}
-            {`https://muhibullahlollygift.netlify.app/lollies/${data.getLollyByPath.lollyPath}`}
+            {`/lollies/${data.getLollyByPath.lollyPath}`}
           </span>
           <div className="recievedContentContainer">
             <Lolly
