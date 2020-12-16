b9127d15f8c14a86fdcb9724e34af36a7d4a8b02
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-12-16 07:41:10 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: "gatsby-source-graphql",
+      resolve: `gatsby-source-graphql`,
       options: {
         typeName: "Lolly",
         fieldName: "lolly",
